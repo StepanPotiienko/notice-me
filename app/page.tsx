43bd22fd8af865d9393e042b271a549a77a7c35c
Stepan Potiienko <kotnexjieb@protonmail.com>
2024-08-13 20:05:40 +0300

    i forgor to move app folder

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { List } from "./components/List";
+
+export default function Home() {
+  // Here all notifications go. They should be displayed in a list, each of them being in a box.
+  // Kind of like Apple did it.
+  return (
+    <>
+      <List />
+    </>
+  );
+}
